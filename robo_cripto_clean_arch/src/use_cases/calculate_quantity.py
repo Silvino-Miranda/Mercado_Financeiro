8cf5b9055b50eeb7b03685ad19c9a657dc42b9b9
Silvino Miranda <silvino_miranda@hotmail.com>
2024-12-05 00:18:05 -0300

    Implement Binance client using CCXT and update quantity calculation logic

--- robo_cripto_clean_arch/src/use_cases/calculate_quantity.py
+++ robo_cripto_clean_arch/src/use_cases/calculate_quantity.py
@@ -1,23 +1,23 @@
 # use_cases/calculate_quantity.py
 import os
 from dotenv import load_dotenv
-from frameworks_drivers.binance_client import binance_client
+from frameworks_drivers.binance_client_ccxt import binance_client_ccxt
 
 load_dotenv()
 
 def calculate_quantity_available(symbol):
-    balance = binance_client.fetch_balance()
+    balance = binance_client_ccxt.fetch_balance()
     usdt_balance = float(balance["USDT"]["free"])
 
     if usdt_balance <= 0.0:
         print(f"Saldo insuficiente. Saldo disponível: {usdt_balance} USDT")
         return False
 
-    ticker = binance_client.fetch_ticker(symbol)
+    ticker = binance_client_ccxt.fetch_ticker(symbol)
     price = ticker["last"]
 
     amount_crypto = usdt_balance / price
-    amount_crypto = binance_client.amount_to_precision(symbol, amount_crypto)
+    amount_crypto = binance_client_ccxt.amount_to_precision(symbol, amount_crypto)
     amount_crypto = float(amount_crypto)
 
     print(f"Quantidade de {symbol} a ser comprada: {amount_crypto}")
